ab3ba6bec879cdd65468ec32247eaaa39d8faa06
ThemSatan <themsatan@email.cz>
2024-09-17 19:09:35 +0200

    laravel struggle

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="Saquib" content="Blade">
+<title>Checkout our layout</title>
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
